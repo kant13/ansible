c134199e345079640607cc86cbc45f0de8b42db7
Александр Сысоев <asysoev@lanit.ru>
2018-03-29 18:12:30 +0300

    add utf-8

--- lib/ansible/plugins/connection/winrm.py
+++ lib/ansible/plugins/connection/winrm.py
@@ -407,7 +407,11 @@ class Connection(ConnectionBase):
             # ensure stdout/stderr are text for py3
             # FUTURE: this should probably be done internally by pywinrm
             response = Response(tuple(to_text(v) if isinstance(v, binary_type) else v for v in resptuple))
-            display.vvvvv('response111 %r' %response.std_out)
+            response1 = Response(tuple(to_text(v, encoding='cp866') if isinstance(v, binary_type) else v for v in resptuple))
+            display.vvvvv('response111 %r' %response1.std_out)
+            response2 = Response(tuple(to_text(v, encoding='windows-1251') if isinstance(v, binary_type) else v for v in resptuple))
+            display.vvvvv('response222 %r' %response2.std_out)
+
             # TODO: check result from response and set stdin_push_failed if we have nonzero
             if from_exec:
                 display.vvvvv('WINRM RESULT %r' % to_text(response), host=self._winrm_host)
